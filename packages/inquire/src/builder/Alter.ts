0af08437d79b8b445d4cbd53ff3de3702496c922
Chris Blanquera <chris@incept.asia>
2024-12-08 21:53:01 +0800

    initial commit

---
+++ packages/inquire/src/builder/Alter.ts
@@ -0,0 +1,154 @@
+import type { 
+  Field, 
+  AlterFields, 
+  AlterKeys, 
+  AlterUnqiues, 
+  AlterPrimaries,
+  DatabaseEngine
+} from '../types';
+
+export default class Alter {
+  /**
+   * Database engine
+   */
+  public readonly engine: DatabaseEngine;
+
+  /**
+   * List of fields
+   */
+  protected _fields: AlterFields = { add: {}, update: {}, remove: [] };
+
+  /**
+   * List of key indexes
+   */
+  protected _keys: AlterKeys = { add: {}, remove: [] };
+
+  /**
+   * The table to create.
+   */
+  protected _table: string;
+
+  /**
+   * List of primary keys
+   */
+  protected _primary: AlterPrimaries = { add: [], remove: [] };
+
+  /**
+   * List of unique keys
+   */
+  protected _unique: AlterUnqiues = { add: {}, remove: [] };
+
+  /**
+   * Converts the class data to object
+   */
+  public get build() {
+    return {
+      fields: this._fields,
+      keys: this._keys,
+      primary: this._primary,
+      table: this._table,
+      unique: this._unique
+    }
+  }
+
+  /**
+   * Convert the builder to a query object.
+   */
+  public get query() {
+    return this.engine.dielect.alter(this);
+  }
+
+  /**
+   * Set table, quote and action
+   */
+  public constructor(table: string, engine: DatabaseEngine) {
+    this._table = table;
+    this.engine = engine;
+  }
+
+  /**
+   * Add a field to the table.
+   */
+  public addField(name: string, field: Field) {
+    this._fields.add[name] = field;
+    return this;
+  }
+
+  /**
+   * Add a key index to the table.
+   */
+  public addKey(name: string, field: string|string[]) {
+    if (!Array.isArray(field)) {
+      field = [field];
+    }
+    this._keys.add[name] = field;
+    return this;
+  }
+
+  /**
+   * Add a primary key to the table.
+   */
+  public addPrimaryKey(name: string) {
+    this._primary.add.push(name);
+    return this;
+  }
+
+  /**
+   * Add a unique key to the table.
+   */
+  public addUniqueKey(name: string, field: string|string[]) {
+    if (!Array.isArray(field)) {
+      field = [field];
+    }
+    this._unique.add[name] = field;
+    return this;
+  }
+
+  /**
+   * Update a field in the table.
+   */
+  public changeField(name: string, field: Field) {
+    this._fields.update[name] = field;
+    return this;
+  }
+
+  /**
+   * Remove a field from the table.
+   */
+  public removeField(name: string) {
+    this._fields.remove.push(name);
+    return this;
+  }
+
+  /**
+   * Remove a key index from the table.
+   */
+  public removeKey(name: string) {
+    this._keys.remove.push(name)
+    return this;
+  }
+
+  /**
+   * Add a primary key to the table.
+   */
+  public removePrimaryKey(name: string) {
+    this._primary.remove.push(name);
+    return this;
+  }
+
+  /**
+   * Remove a unique key from the table.
+   */
+  public removeUniqueKey(name: string) {
+    this._unique.remove.push(name)
+    return this;
+  }
+
+  /**
+   * Makes class awaitable. Should get the 
+   * query and values and call the action.
+   */
+  public then() {
+    return this.engine.query([ this.query ]);
+  }
+}
---
+++ packages/inquire/src/dialect/Mysql.ts
@@ -0,0 +1,345 @@
+import { Join, Value, FlatValue, Dialect } from '../types';
+
+import type Alter from '../builder/Alter';
+import type Create from '../builder/Create';
+import type Delete from '../builder/Delete';
+import type Insert from '../builder/Insert';
+import type Select from '../builder/Select';
+import type Update from '../builder/Update';
+
+import Exception from '../Exception';
+
+const joins = {
+  inner: 'INNER',
+  left: 'LEFT',
+  left_outer: 'LEFT OUTER',
+  right: 'RIGHT',
+  right_outer: 'RIGHT OUTER',
+  full: 'FULL',
+  full_outer: 'FULL OUTER',
+  cross: 'CROSS'
+};
+
+export default class Mysql implements Dialect {
+  // The character used to quote identifiers.
+  public static readonly quote = '`';
+  
+  /**
+   * Converts alter builder to query and values
+   */
+  public alter(builder: Alter) {
+    const q = Mysql.quote;
+    const build = builder.build;
+    const query: string[] = [];
+
+    const removeFields = build.fields.remove.map(
+      name => `DROP ${q}${name}${q}`
+    );
+
+    const addFields = Object.keys(build.fields.add).map(name => {
+      const field = build.fields.add[name];
+      const column: string[] = [];
+      column.push(`${q}${name}${q}`);
+      field.type && column.push(field.type);
+      field.length && column.push(`(${field.length})`);
+      field.attribute && column.push(field.attribute);
+      field.unsigned && column.push('UNSIGNED');
+      field.nullable && column.push('NOT NULL');
+      field.autoIncrement && column.push('AUTO_INCREMENT');
+      if (field.default) {
+        if (!isNaN(Number(field.default))) {
+          column.push(`DEFAULT ${field.default}`);
+        } else {
+          column.push(`DEFAULT '${field.default}'`);
+        }
+      } else if (field.nullable) {
+        column.push('DEFAULT NULL');
+      }
+
+      return `ADD COLUMN ${column.join(' ')}`;
+    });
+
+    const changeFields = Object.keys(build.fields.update).map(name => {
+      const field = build.fields.add[name];
+      const column: string[] = [];
+      column.push(`${q}${name}${q}`);
+      field.type && column.push(field.type);
+      field.length && column.push(`(${field.length})`);
+      field.attribute && column.push(field.attribute);
+      field.unsigned && column.push('UNSIGNED');
+      field.nullable && column.push('NOT NULL');
+      field.autoIncrement && column.push('AUTO_INCREMENT');
+      if (field.default) {
+        if (!isNaN(Number(field.default))) {
+          column.push(`DEFAULT ${field.default}`);
+        } else {
+          column.push(`DEFAULT '${field.default}'`);
+        }
+      } else if (field.nullable) {
+        column.push('DEFAULT NULL');
+      }
+
+      return `CHANGE COLUMN ${column.join(' ')}`;
+    });
+
+    const removePrimaries = build.primary.remove.map(
+      name => `DROP PRIMARY KEY ${q}${name}${q}`
+    );
+
+    const addPrimaries = `ADD PRIMARY KEY (${q}${build.primary.remove.join(`${q}, ${q}`)}${q})`;
+
+    const removeUniques = build.unique.remove.map(
+      name => `DROP UNIQUE ${q}${name}${q}`
+    );
+
+    const addUniques = Object.keys(build.unique.add).map(
+      key => `ADD UNIQUE ${q}${key}${q} (${q}${build.unique.add[key].join(`${q}, ${q}`)}${q})`
+    );
+
+    const removeKeys = build.keys.remove.map(
+      name => `DROP INDEX ${q}${name}${q}`
+    );
+
+    const addKeys = Object.keys(build.keys.add).map(
+      key => `ADD INDEX ${q}${key}${q} (${q}${build.unique.add[key].join(`${q}, ${q}`)}${q})`
+    );
+
+    if (!removeFields.length
+      && !addFields.length
+      && !changeFields.length
+      && !removePrimaries.length
+      && !addPrimaries.length
+      && !removeUniques.length
+      && !addUniques.length
+      && !removeKeys.length
+      && !addKeys.length
+    ) {
+      throw Exception.for('No alterations made.')
+    }
+
+    query.push(
+      ...removeFields,
+      ...addFields,
+      ...changeFields,
+      ...removePrimaries,
+      ...addPrimaries,
+      ...removeUniques,
+      ...addUniques,
+      ...removeKeys,
+      ...addKeys
+    );
+    return { 
+      query: `ALTER TABLE ${build.table} (${query.join(', ')})`, 
+      values: [] 
+    };
+  }
+
+  /**
+   * Converts create builder to query and values
+   */
+  public create(builder: Create) {
+    const q = Mysql.quote;
+    const build = builder.build;
+    if (!build.fields.length) {
+      throw Exception.for('No fields provided');
+    }
+
+    const query: string[] = [];
+
+    const fields = Object.keys(build.fields).map(name => {
+      const field = build.fields[name];
+      const column: string[] = [];
+      column.push(`${q}${name}${q}`);
+      field.type && column.push(field.type);
+      field.length && column.push(`(${field.length})`);
+      field.attribute && column.push(field.attribute);
+      field.unsigned && column.push('UNSIGNED');
+      field.nullable && column.push('NOT NULL');
+      field.autoIncrement && column.push('AUTO_INCREMENT');
+      if (field.default) {
+        if (!isNaN(Number(field.default))) {
+          column.push(`DEFAULT ${field.default}`);
+        } else {
+          column.push(`DEFAULT '${field.default}'`);
+        }
+      } else if (field.nullable) {
+        column.push('DEFAULT NULL');
+      }
+
+      return column.join(' ');
+    }).join(', ');
+
+    query.push(fields);
+  
+    if (build.primary.length) {
+      query.push(`, PRIMARY KEY (${build.primary
+        .map(key => `${q}${key}${q}`)
+        .join(', ')})`
+      );
+    }
+
+    if (build.unique.length) {
+      query.push(Object.keys(build.unique).map(
+        key => `, UNIQUE KEY ${q}${key}${q} (${q}${build.unique[key].join(`${q}, ${q}`)}${q})`
+      ).join(', '));
+    }
+
+    if (build.keys.length) {
+      query.push(Object.keys(build.keys).map(
+        key => `, KEY ${q}${key}${q} (${q}${build.keys[key].join(`${q}, ${q}`)}${q})`
+      ).join(', '));
+    }
+
+    return { 
+      query: `CREATE TABLE IF NOT EXISTS ${build.table} (${query.join(' ')})`, 
+      values: [] 
+    };
+  }
+
+  /**
+   * Converts delete builder to query and values
+   */
+  public delete(builder: Delete) {
+    const q = Mysql.quote;
+    const build = builder.build;
+    if (!build.filters.length) {
+      throw Exception.for('No filters provided');
+    }
+
+    const query: string[] = [];
+    const values: FlatValue[] = [];
+    query.push(`DELETE FROM ${q}${build.table}${q}`);
+
+    const filters = build.filters.map(filter => {
+      values.push(...filter[1]);
+      return filter[0];
+    }).join(' AND ');
+    query.push(`WHERE ${filters}`);
+
+    return { query: query.join(' '), values };
+  }
+
+  /**
+   * Converts insert builder to query and values
+   */
+  public insert(builder: Insert) {
+    const q = Mysql.quote;
+    const build = builder.build;
+    if (build.values.length === 0) {
+      throw Exception.for('No values provided');
+    }
+
+    const query: string[] = [];
+    const values: Value[] = [];
+    
+    query.push(`INSERT INTO ${q}${build.table}${q}`);
+
+    const keys = Object.keys(build.values[0]);
+    query.push(`(${q}${keys.join(`${q}, ${q}`)}${q})`);
+
+    const row = build.values.map((value) => {
+      const row = keys.map(key => value[key]);
+      values.push(...row);
+      return `(${row.map(() => '?').join(', ')})`;
+    });
+
+    query.push(`VALUES ${row.join(', ')}`);
+    return { query: query.join(' '), values };
+  }
+
+  /**
+   * Converts select builder to query and values
+   */
+  public select(builder: Select) {
+    const q = Mysql.quote;
+    const build = builder.build;
+    if (!build.table) {
+      throw Exception.for('No table specified');
+    }
+
+    const query: string[] = [];
+    const values: FlatValue[] = [];
+
+    const columns = build.columns
+      .map(column => column.split(' '))
+      .flat(1)
+      .map(column => `${q}${column.split('.').join('`.`')}${q}`);
+
+    query.push(`SELECT ${columns.join(', ')}`);
+    if (build.table) {
+      if (build.table[1] !== build.table[0]) {
+        query.push(`FROM ${q}${build.table[0]}${q} AS ${q}${build.table[1]}${q}`);
+      } else {
+        query.push(`FROM ${q}${build.table[0]}${q}`);
+      }
+    }
+
+    if (build.relations.length) {
+      const relations = build.relations.map(relation => {
+        const type = relation.type as Join;
+        const table = relation.table !== relation.as 
+          ? `${q}${relation.table}${q} AS ${q}${relation.as}${q}`
+          : `${q}relation.table${q}`;
+        return `${joins[type]} ${table} ON (${q}${relation.from}${q} = ${q}${relation.to}${q})`;
+      });
+      query.push(relations.join(' '));
+    }
+
+    if (build.filters.length) {
+      const filters = build.filters.map(filter => {
+        values.push(...filter[1]);
+        return filter[0];
+      }).join(' AND ');
+      query.push(`WHERE ${filters}`);
+    }
+
+    if (build.sort.length) {
+      const sort = build.sort.map((sort) => `${sort[0]} ${sort[1]}`);
+      query.push(`ORDER BY ${q}${sort.join(`${q}, ${q}`)}${q}`);
+    }
+
+    if (build.limit) {
+      query.push(`LIMIT ${build.limit}`);
+    }
+
+    if (build.offset) {
+      query.push(`OFFSET ${build.offset}`);
+    }
+
+    return { query: query.join(' '), values };
+  }
+
+  /**
+   * Converts update builder to query and values
+   */
+  public update(builder: Update) {
+    const q = Mysql.quote;
+    const build = builder.build;
+    if (!Object.keys(build.data).length) {
+      throw Exception.for('No data provided');
+    }
+
+    const query: string[] = [];
+    const values: Value[] = [];
+
+    query.push(`UPDATE ${q}${build.table}${q}`);
+
+    if (Object.keys(build.data).length) {
+      const data = Object.keys(build.data).map(key => {
+        values.push(build.data[key]);
+        return `${key} = ?`;
+      }).join(', ');
+      query.push(`SET ${data}`);
+    }
+
+    if (build.filters.length) {
+      const filters = build.filters.map(filter => {
+        values.push(...filter[1]);
+        return filter[0];
+      }).join(' AND ');
+      query.push(`WHERE ${filters}`);
+    }
+
+    return { query: query.join(' '), values };
+  }
+}
