808c1be566032bb0d0e020ab866a07bc143c1fc7
Chris Blanquera <chris@incept.asia>
2024-12-09 23:45:22 +0800

    updated ignore

---
+++ examples/with-better-sqlite3/src/index.ts
@@ -0,0 +1,70 @@
+import { Client, Pool } from 'pg';
+import Engine from '@stackpress/inquire/dist/Engine';
+import PGConnection from './Connection';
+
+async function main(usePool = true) {
+  //this is the raw resource, anything you want
+  const resource = usePool 
+    ? await (async () => {
+      const pool = new Pool({
+        database: 'inquire',
+        user: 'postgres'
+      });
+      return await pool.connect();
+    })() 
+    : await (async () => {
+      const resource = new Client({
+        database: 'inquire',
+        user: 'postgres'
+      });
+      await resource.connect();
+      return resource;
+    })();
+
+  //this maps the resource to the engine
+  const db = new PGConnection(resource);
+  //this is the final engine that you will use to interact with the database
+  const engine = new Engine(db);
+
+  const create = engine.create('profile')
+    .addField('id', { type: 'VARCHAR', length: 255 })
+    .addField('name', { type: 'VARCHAR', length: 255 })
+    .addPrimaryKey('id');
+  console.log(create.query);
+  console.log(await create);
+
+  const insert = engine
+    .insert('profile')
+    .values({ id: '1', name: 'John Doe' });
+  console.log(insert.query);
+  console.log(JSON.stringify(await insert, null, 2));
+
+  const select = engine.select('*').from('profile');
+  console.log(select.query);
+  console.log(JSON.stringify(await select, null, 2));
+
+  const update = engine
+    .update('profile')
+    .set({ name: 'Jane Doe' })
+    .where('id = ?', [ '1' ]);
+  console.log(update.query);
+  console.log(JSON.stringify(await update, null, 2));
+  console.log(JSON.stringify(await select, null, 2));
+
+  const remove = engine
+    .delete('profile')
+    .where('id = ?', [ '1' ]);
+  console.log(remove.query);
+  console.log(JSON.stringify(await remove, null, 2));
+  console.log(JSON.stringify(await select, null, 2));
+
+  if (usePool && resource instanceof Client === false) {
+    resource.release();
+  }
+
+  if (resource instanceof Client) {
+    await resource.end();
+  }
+}
+
+main(true).catch(console.error);
