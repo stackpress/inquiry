bf03dee6ee85c6604fcd7a4d0037ee913f6d3d80
Rheilly Aguilar <rheilly17@gmail.com>
2024-12-18 11:16:30 +0800

    cover the uncovered lines Insert, Select and Update

--- packages/inquire/tests/Insert.test.ts
+++ packages/inquire/tests/Insert.test.ts
@@ -2,6 +2,8 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
 import Insert from '../src/builder/Insert';
+import Engine from '../src/Engine';
+import Exception from '../src/Exception';
 
 describe('Insert Builder Tests', () => {
   it('Should build insert', async () => {
@@ -17,4 +19,58 @@ describe('Insert Builder Tests', () => {
     expect(build.values[1].id).to.equal(2);
     expect(build.values[1].name).to.equal('barfoo');
   });
+
+
+  // Line 26 - 33
+  it('Should handle setting and getting the engine', () => {
+    const insert = new Insert('table');
+    const mockEngine = {} as Engine;
+    expect(insert.engine).to.be.undefined;
+    insert.engine = mockEngine;
+    expect(insert.engine).to.equal(mockEngine);
+    insert.engine = undefined;
+    expect(insert.engine).to.be.undefined;
+  });
+
+  // Line 58 - 73
+  it('Should throw an exception when dialect is not provided and engine is undefined', () => {
+    const insert = new Insert('table');
+    expect(() => insert.query()).to.throw('No dialect provided');
+  });
+
+  // Line 58 - 73
+  it('Should return a promise when query method is called with a valid engine', () => {
+    const mockDialect = {
+      insert: () => 'mock query' 
+    };
+    const mockEngine = {
+      query: () => Promise.resolve(['result']),
+      dialect: mockDialect
+    } as unknown as Engine;
+    const insert = new Insert('table', mockEngine);
+    const result = insert.then((res) => res);
+    expect(result).to.be.a('promise');
+    return result.then((res) => {
+      expect(res).to.deep.equal(['result']);
+    });
+  });
+  
+  // Line 58 - 73
+  it('Should throw an exception when no engine is provided', () => {
+    const insert = new Insert('table', undefined as unknown as Engine);
+    expect(() => insert.then((res) => res)).to.throw(Exception, 'No engine provided');
+  });
+
+  // Line 78
+  it('Should handle a single record object as input and convert it to an array', () => {
+    const insert = new Insert('table');
+    const singleRecord = { id: 1, name: 'single' };
+    insert.values(singleRecord);
+    const build = insert.build();
+    expect(build.values).to.be.an('array');
+    expect(build.values).to.have.lengthOf(1);
+    expect(build.values[0]).to.deep.equal(singleRecord);
+  });
+
+
 });
--- packages/inquire/tests/Update.test.ts
+++ packages/inquire/tests/Update.test.ts
@@ -2,6 +2,8 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
 import Update from '../src/builder/Update';
+import Engine from '../src/Engine';
+import Exception from '../src/Exception';
 
 describe('Update Builder Tests', () => {
   it('Should build update', async () => {
@@ -16,4 +18,58 @@ describe('Update Builder Tests', () => {
 
     expect(true).to.be.true;
   });
+
+  // Line 36 - 40
+  it('Should handle setting and getting the engine', () => {
+    const update = new Update('table');
+    const mockEngine = {} as Engine;
+    expect(update.engine).to.be.undefined;
+    update.engine = mockEngine;
+    expect(update.engine).to.equal(mockEngine);
+    update.engine = undefined;
+    expect(update.engine).to.be.undefined;
+  });
+
+  // Line 69 - 73
+  it('Should throw an exception when dialect is not provided and engine is undefined', () => {
+    const update = new Update('table');
+    expect(() => update.query()).to.throw('No dialect provided');
+  });
+
+  // Line 89 - 92
+  it('Should return a promise when query method is called with a valid engine', () => {
+    const mockDialect = {
+      update: () => 'mock query' 
+    };
+    const mockEngine = {
+      query: () => Promise.resolve(['result']),
+      dialect: mockDialect
+    } as unknown as Engine;
+    const update = new Update('table', mockEngine);
+    const result = update.then((res) => res);
+    expect(result).to.be.a('promise');
+    return result.then((res) => {
+      expect(res).to.deep.equal(['result']);
+    });
+  });
+
+  // Line 90
+  it('Should throw an exception when no engine is provided', () => {
+    const update = new Update('table', undefined as unknown as Engine);
+    expect(() => update.then((res) => res)).to.throw(Exception, 'No engine provided');
+  });
+
+  // Line 98
+  it('Should add a filter with an empty query string and no values', () => {
+    const update = new Update('table');
+    update.where('');
+    const filters = update.build().filters;
+    expect(filters).to.have.lengthOf(1);
+    expect(filters[0][0]).to.equal('');
+    expect(filters[0][1]).to.deep.equal([]);
+  });
+ 
+
+
+
 });
