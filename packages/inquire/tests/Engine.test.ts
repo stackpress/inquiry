735b1bce3ca42392667b8c9074833b356cdd0bdc
Rheilly Aguilar <rheilly17@gmail.com>
2024-12-19 00:10:05 +0800

    cover the uncoverlines Engine Test

--- packages/inquire/tests/Engine.test.ts
+++ packages/inquire/tests/Engine.test.ts
@@ -5,16 +5,20 @@ import type { Connection, QueryObject, Transaction } from '../src/types';
 import Pgsql from '../src/dialect/Pgsql';
 import Engine from '../src/Engine';
 import Exception from '../src/Exception';
+import Delete from '../src/builder/Delete';
+import Insert from '../src/builder/Insert';
+import Select from '../src/builder/Select';
+import Update from '../src/builder/Update';
 
 describe('Engine Tests', () => {
   it('Should diff table schemas', async () => {
     const resource = new MockConnection();
     const engine = new Engine(resource);
     const from = engine.create('profile')
-      .addField('id', { 
-        type: 'INTEGER', 
-        length: 11, 
-        autoIncrement: true 
+      .addField('id', {
+        type: 'INTEGER',
+        length: 11,
+        autoIncrement: true
       })
       .addField('name', { type: 'varchar', length: 255 })
       .addField('bio', { type: 'varchar', length: 255 })
@@ -25,11 +29,11 @@ describe('Engine Tests', () => {
       .addKey('active', 'active');
 
     const to = engine.create('profile')
-      .addField('id', { 
-        type: 'INTEGER', 
-        length: 10, 
-        unsigned: true, 
-        autoIncrement: true 
+      .addField('id', {
+        type: 'INTEGER',
+        length: 10,
+        unsigned: true,
+        autoIncrement: true
       })
       .addField('name', { type: 'varchar', length: 255 })
       //ADD COLUMN "age" INTEGER NOT NULL
@@ -44,7 +48,7 @@ describe('Engine Tests', () => {
       .addPrimaryKey('id')
       //[same]
       .addUniqueKey('name', 'name');
-      //DROP INDEX "active"
+    //DROP INDEX "active"
     const alter = engine.diff(from, to);
     expect(alter.query()[0].query).to.equal(`ALTER TABLE "profile" `
       + `ADD COLUMN "age" INTEGER NOT NULL, `
@@ -54,6 +58,224 @@ describe('Engine Tests', () => {
       + `ALTER COLUMN "active" SET DEFAULT TRUE, `
       + `DROP INDEX "active"`);
   });
+
+  // Line 50
+  it('Should create a new Delete instance when delete method is called with a valid table name', () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const deleteInstance = engine.delete('users');
+    expect(deleteInstance).to.be.instanceOf(Delete);
+  });
+
+  // Line 68 - 69
+  it('Should remove a field from "from" schema when it does not exist in "to" schema', () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const from = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true })
+      .addField('name', { type: 'varchar', length: 255 })
+      .addField('bio', { type: 'varchar', length: 255 });
+    const to = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true })
+      .addField('name', { type: 'varchar', length: 255 });
+    const alter = engine.diff(from, to);
+    expect(alter.query()[0].query).to.include('DROP COLUMN "bio"');
+  });
+
+  // Line 89
+  it('Should remove primary key from "from" schema when it does not exist in "to" schema', () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const from = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true })
+      .addPrimaryKey('id');
+    const to = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true });
+    const alter = engine.diff(from, to);
+    expect(alter.query()[0].query).to.include('ALTER TABLE "profile" DROP CONSTRAINT');
+  });
+
+
+  // Line 95 - 96
+  it('Should remove unique key from "from" schema when it does not exist in "to" schema', () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const from = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true })
+      .addField('name', { type: 'varchar', length: 255 })
+      .addUniqueKey('name', 'name');
+    const to = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true })
+      .addField('name', { type: 'varchar', length: 255 });
+    const alter = engine.diff(from, to);
+    expect(alter.query()[0].query).to.include('ALTER TABLE "profile" DROP UNIQUE');
+  });
+
+  // Line 100 - 101
+  it('Should update unique key when the unique key definition changes between schemas', () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const from = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true })
+      .addField('email', { type: 'varchar', length: 255 })
+      .addUniqueKey('email', ['email']);
+    const to = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true })
+      .addField('email', { type: 'varchar', length: 255 })
+      .addUniqueKey('email', ['email', 'id']);
+    const alter = engine.diff(from, to);
+    expect(alter.query()[0].query).to.include('ALTER TABLE "profile" DROP UNIQUE');
+  });
+
+  // Line 111 - 113
+  it('Should call alter.removeKey and alter.addKey when jsonCompare returns false for key comparison', () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const from = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true })
+      .addKey('name', ['name']);
+    const to = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true })
+      .addKey('name', ['name', 'id']);
+    const alter = engine.diff(from, to);
+    const query = alter.query()[0].query;
+    expect(query).to.include('ALTER TABLE "profile" DROP INDEX');
+  });
+
+  // Line 118 - 125
+
+  
+  // Line 137
+  it('Should add a primary key when the "to" schema has a primary key not present in the "from" schema', () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const from = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true });
+    const to = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true })
+      .addPrimaryKey('id');
+    const alter = engine.diff(from, to);
+    expect(alter.query()[0].query).to.include('ALTER TABLE "profile" ADD PRIMARY KEY');
+  });
+
+  // Line 143
+  it('Should add a unique key when the "to" schema has a unique key not present in the "from" schema', () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const from = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true });
+    const to = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true })
+      .addField('email', { type: 'varchar', length: 255 })
+      .addUniqueKey('email', 'email');
+    const alter = engine.diff(from, to);
+    expect(alter.query()[0].query).to.include('ALTER TABLE "profile" ADD COLUMN');
+  });
+
+  // Line 149
+  it('Should add a key when the "to" schema has a key not present in the "from" schema', () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const from = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true });
+    const to = engine.create('profile')
+      .addField('id', { type: 'INTEGER', length: 11, autoIncrement: true })
+      .addKey('name', ['name']);
+    const alter = engine.diff(from, to);
+    expect(alter.query()[0].query).to.include('ALTER TABLE "profile" ADD INDEX');
+  });
+
+  // Line 154 - 155
+
+  // Line 166 - 226
+  it('Should drop a table when the drop method is called with a valid table name', async () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const result = await engine.drop('users');
+    expect(result).to.be.an('array').that.is.empty;
+  });
+
+  // Line 166 - 226
+  it('Should rename a table when the rename method is called with valid "from" and "to" table names', async () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const result = await engine.rename('old_table', 'new_table');
+    expect(result).to.be.an('array').that.is.empty;
+  });
+
+  // Line 166 - 226
+  it('Should truncate a table without cascade when the truncate method is called with cascade set to false', async () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const result = await engine.truncate('users', false);
+    expect(result).to.be.an('array').that.is.empty;
+  });
+
+  // Line 166 - 226
+  it('Should truncate a table with cascade when the truncate method is called with cascade set to true', async () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const result = await engine.truncate('users', true);
+    expect(result).to.be.an('array').that.is.empty;
+  });
+
+  // Line 166 - 226
+  it('Should create a new Insert instance when insert method is called with a valid table name', () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const insertInstance = engine.insert('users');
+    expect(insertInstance).to.be.instanceOf(Insert);
+  });
+
+  // Line 166 - 226
+  it('Should create a new Select instance when select method is called with valid column names', () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const selectInstance = engine.select(['id', 'name']);
+    expect(selectInstance).to.be.instanceOf(Select);
+  });
+
+  // Line 166 - 226
+  it('Should create a new Update instance when update method is called with a valid table name', () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const updateInstance = engine.update('users');
+    expect(updateInstance).to.be.instanceOf(Update);
+  });
+
+  // Line 166 - 226
+  it('Should execute a transaction when the transaction method is called with a valid callback', async () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const callback = async (trx: any) => {
+      return [];
+    };
+    const result = await engine.transaction(callback);
+    expect(result).to.be.an('array').that.is.empty;
+  });
+
+  // Line 166 - 226
+  it('Should execute a query with a QueryObject and return the expected results', async () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const queryObject: QueryObject = {
+      query: 'SELECT * FROM users WHERE id = ?',
+      values: [1]
+    };
+    const result = await engine.query(queryObject);
+    expect(result).to.be.an('array').that.is.empty;
+  });
+
+  // Line 166 - 226
+  it('Should execute a query with a string query and values array and return the expected results', async () => {
+    const resource = new MockConnection();
+    const engine = new Engine(resource);
+    const query = 'SELECT * FROM users WHERE id = ?';
+    const values = [1];
+    const result = await engine.query(query, values);
+    expect(result).to.be.an('array').that.is.empty;
+  });
+
 });
 
 class MockConnection implements Connection {
